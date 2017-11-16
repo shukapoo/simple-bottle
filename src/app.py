abc3503aa4a9d4b572fc4ec6322631ee4ca11c34
Olli Tourunen <olli.tourunen@csc.fi>
2017-11-16 14:18:57 +0200

    initial import

---
+++ src/app.py
@@ -0,0 +1,40 @@
+# import key pieces from bottle package
+from bottle import route, run, static_file
+# import os for reading environment variables
+import os
+
+
+def get_static_root():
+    """
+    Return root directory for static content
+    """
+    return os.environ.get('STATIC_CONTENT_ROOT', '/opt/app-root/src/static')
+
+
+@route('/')
+def root_page():
+    """
+    Route for root path, just try to return index.html
+    """
+    return static_file('/index.html', get_static_root())
+
+
+@route('/static/<path:path>')
+def server_static(path):
+    """
+    Any path not defined more specifically is handled as static content
+    """
+    return static_file(path, get_static_root())
+
+
+@route('/healthz')
+def health():
+    """
+    Status endpoint for health checks
+    """
+    return 'OK'
+
+
+if __name__ == '__main__':
+    # Here we simply start the server process
+    run(host='0.0.0.0', port=8080, reloader=True)
